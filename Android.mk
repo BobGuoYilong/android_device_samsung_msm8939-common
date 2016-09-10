896f3f264e20ddb35beae051034b418bd816b92a
Grace5921 <androidlover5842@gmail.com>
2016-09-10 20:30:51 +0530

    typo

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter a7lte, $(TARGET_DEVICE)),)
+ifneq ($(filter a8eltezh, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
